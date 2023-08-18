6fbc7f30c915bd2b0770e1e545a3d837f88a5ca8
Dinesh442000 <dchoudhary442000@gmail.com>
2023-08-18 22:15:34 +0530

    high level comp

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -10,19 +10,30 @@ const RestaurantCard = (props) => {
   // console.log(resData?.info);
   //console.log(sla);
   return (
-    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+    <div className="m-4 p-4 w-56 rounded-lg bg-gray-100 hover:bg-gray-300">
       <img
-        className="res-logo"
+        className="rounded-lg"
         alt="res-logo"
         src={CDN_URL + cloudinaryImageId}
       />
-      <h3>{name}</h3>
+      <h3 className="font-bold py-4 text-lg">{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating} stars</h4>
       <h4>{costForTwo} </h4>
       <h4>{sla.deliveryTime} minutes</h4>
     </div>
   );
+};
+
+export const withPromotedlabel = (RestaurantCard) => {
+  return () => {
+    return () => {
+      <div>
+        <RestaurantCard />
+        <label>Promoted</label>
+      </div>;
+    };
+  };
 };
 
 export default RestaurantCard;
