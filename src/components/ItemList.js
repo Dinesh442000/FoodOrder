7941bf375268129f8ff1592e69043852f54fb903
Dinesh442000 <dchoudhary442000@gmail.com>
2023-08-26 21:07:10 +0530

    add items

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,8 +1,17 @@
+import { useDispatch } from "react-redux";
 import { ListItemImages } from "../utils/constants";
+import { addItem } from "../utils/cartSlice";
 
 const ItemList = ({ items }) => {
-  console.log(ListItemImages + items[0]?.card?.info?.imageId);
-  console.log(items[0]?.card?.info);
+  // console.log(ListItemImages + items[0]?.card?.info?.imageId);
+  // console.log(items[0]?.card?.info);
+
+  const dispatch = useDispatch();
+
+  const handleAddItem = (item) => {
+    dispatch(addItem(item));
+  };
+
   return (
     <div>
       <ul>
@@ -20,7 +29,10 @@ const ItemList = ({ items }) => {
             </div>
             <div className="w-3/12 p-4">
               <div className="absolute">
-                <button className="p-2 mx-16 rounded-lg bg-black text-white shadow-lg">
+                <button
+                  className="p-2 mx-16 rounded-lg bg-black text-white shadow-lg"
+                  onClick={() => handleAddItem(item)}
+                >
                   {" "}
                   Add+
                 </button>
